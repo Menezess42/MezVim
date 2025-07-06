648d3f0f94c821e7d7b6fb62467dfe6ece79a792
Menezess42 <arielmenezess@hotmail.com>
2025-07-06 20:19:12 -0300

    flake updata

--- lua/m42/plugins/init.lua
+++ lua/m42/plugins/init.lua
@@ -38,9 +38,4 @@ require('lze').load{
         },
     },
     {import = "m42.plugins.ufo",},
-    -- { import = "m42.plugins.molten",},
-    {import = "m42.plugins.images",},
-    {import = "m42.plugins.quarto",},
-    {import = "m42.plugins.repl",},
-
 }
